ad7d3c1170d594e1122f2ffa8397874ead7e338d
Miguel <armendariz.132110@e.unavarra.es>
2022-02-21 09:38:52 +0100

    primer factors

---
+++ Ejercicios/PrimeFactores/PrimeFactorsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
+{
+    /**
+     * @setUp
+     * */
+    protected function setUp(): void
+    {
+    //para instanciar elementos que se van a usar en los test
+    parent::setUp();//
+    }
+}
